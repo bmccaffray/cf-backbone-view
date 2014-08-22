ff465556ecabdd03bb1cb1facf15f091b66f16cf
Brett McCaffray <Brett@bmp.local>
2014-08-22 10:04:12 -0700

    add/edit install files

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt){
+	//require('matchdep').filterdev('grunt-*').foreach(grunt.loadnpmtasks)
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-browserify');
+
+	grunt.initConfig({
+		clean: {
+			dev: {
+				src: ['build/']
+			}
+		},
+		copy: {
+			dev: {
+				expand: true,
+				cwd: 'app/',
+				src: ['*.html', '*.css'],
+				dest: 'build/',
+				filter: 'isFile'
+			}
+		},
+		browserify: {
+			dev: {
+				options: {
+					transform: ['debowerify', 'hbsfy'],
+					debug: true
+				},
+				src: ['app/js/**/*.js'],
+				dest: 'build/bundle.js'
+			}
+		}
+	});
+
+	grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
+}
